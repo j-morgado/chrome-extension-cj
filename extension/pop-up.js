c2641add1b77fcfe21444b2c49c51fe9b655c452
Joao Morgado <jmrmorgado@gmail.com>
2025-05-22 10:17:14 -0400

    init(github): Adding repo to GitHub to work with Cate

--- extension/pop-up.js
+++ extension/pop-up.js
@@ -1,28 +1,9 @@
-const button = document.querySelector('button');
-
 function clip(str) {
     const newDiv = document.createElement('div');
     newDiv.textContent = str;
     document.body.appendChild(newDiv);
 }
 
-button.addEventListener('click', async () => {
-    const [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-    });
-
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: getSelected,
-    });
-});
-
-function getSelected() {
-    chrome.storage.local.set({ 0: window.getSelection().toString() });
-
-}
-
-chrome.storage.local.get(['0']).then((res) => {
+ chrome.storage.local.get(['0']).then((res) => {
     clip(res[0]);
 });
--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,5 +1,23 @@
-chrome.runtime.onMessage.addListener((msg, sender) => {
-    chrome.runtime.sendMessage({
-        message: msg.message,
-    });
+// chrome.runtime.onMessage.addListener((msg, sender) => {
+//     chrome.runtime.sendMessage({
+//         message: msg.message,
+//     });
+// });
+
+chrome.commands.onCommand.addListener(async (command) => {
+    if (command === 'clip') {
+        const [tab] = await chrome.tabs.query({
+            active: true,
+            currentWindow: true,
+        });
+
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: getSelected,
+        });
+    }
+
+    function getSelected() {
+        chrome.storage.local.set({ 0: window.getSelection().toString() });
+    }
 });
